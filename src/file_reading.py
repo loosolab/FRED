504db0bc5ff2a1ac9be6430242cbc61deb57c13f
Jasmin Walter <jasmin.walter@mni.thm.de>
2022-03-15 11:21:18 +0100

    added validation for mandatory keys + comments

--- src/file_reading.py
+++ src/file_reading.py
@@ -1,28 +1,44 @@
 import os
 from src.utils import read_in_yaml
+from src import test_yaml
 
-# from Mampok -> changed mamplan to metafile
+# The following functions were inspired by Mampok and slightly customized
+# https://gitlab.gwdg.de/loosolab/software/mampok/-/blob/master/mampok/
+# file_reading.py
+
+
 def iterate_dir_metafiles(path_metafiles):
+    """
+    iterate through a list of paths to find all _metadata.yaml(yml) files
+    :param path_metafiles: list of paths containing yaml files
+    :return: metafile_list: list of dictionaries containing information from
+             found metadata files
+    """
+
     # turn string into list
     if isinstance(path_metafiles, str):
         path_metafiles = [path_metafiles]
+
     # iterate through paths, through directories, through files
     metafile_list = []
     for path_metafile in path_metafiles:
         for subdir, dirs, files in os.walk(path_metafile):
             for file in files:
-                try:
-                    # add files with suffix 'metadata.y(a)ml'
-                    if file.lower().endswith(
-                            "metadata.yaml") or file.lower().endswith(
-                            'metadata.yml'):
-                        ypath = os.path.join(subdir, file)
-                        # read these yaml as dict and append to metafile-list:
-                        metafile = read_in_yaml(ypath)
-                        metafile['path'] = ypath  # add path to dic
-                        if metafile:
-                            metafile_list.append(metafile)
-                except Exception as e:
-                    print("Problem with YAML format or entries!")
 
+                #TODO: add exceptions
+                #TODO: add test for whitelist
+
+                # add files with suffix '_metadata.y(a)ml'
+                if file.lower().endswith("_metadata.yaml") or file.lower().endswith('_metadata.yml'):
+                    ypath = os.path.join(subdir, file)
+                    # read these yaml as dict and append to metafile-list:
+                    metafile = read_in_yaml(ypath)
+                    # test if metafile is valid
+                    valid = test_yaml.test_for_mandatory(metafile)
+                    if not valid:
+                        print('Missing mandatory key!')
+                    # add path to dic
+                    metafile['path'] = ypath
+                    if metafile:
+                        metafile_list.append(metafile)
     return metafile_list
