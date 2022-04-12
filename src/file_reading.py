078d2813784791fd3858efb731c9798c33da2955
Jasmin Walter <jasmin.walter@mni.thm.de>
2022-04-12 12:05:00 +0200

    integrated validation

--- src/file_reading.py
+++ src/file_reading.py
@@ -2,6 +2,7 @@ import os
 from src.utils import read_in_yaml
 from src import validate_yaml
 
+
 # The following functions were inspired by Mampok and slightly customized
 # https://gitlab.gwdg.de/loosolab/software/mampok/-/blob/master/mampok/
 # file_reading.py
@@ -24,24 +25,23 @@ def iterate_dir_metafiles(path_metafiles):
     for path_metafile in path_metafiles:
         for subdir, dirs, files in os.walk(path_metafile):
             for file in files:
-
-                #TODO: add exceptions
-                #TODO: validation for whitelists
-
                 # add files with suffix '_metadata.y(a)ml'
-                if file.lower().endswith("_metadata.yaml") or file.lower().endswith('_metadata.yml'):
+                if file.lower().endswith(
+                        "_metadata.yaml") or file.lower().endswith(
+                        '_metadata.yml'):
                     ypath = os.path.join(subdir, file)
                     # read these yaml as dict and append to metafile-list:
                     metafile = read_in_yaml(ypath)
                     # test if metafile is valid
-                    valid = validate_yaml.test_for_mandatory(metafile)
-                    if not valid:
-                        print('Missing mandatory key!')
-                    valid_keys = validate_yaml.test_for_valid_keys(metafile)
-                    if not valid_keys:
-                        print('Invalid key found')
+                    valid, missing_mandatory_keys, invalid_keys, \
+                        invalid_entries = validate_yaml.validate_file(metafile)
                     # add path to dic
                     metafile['path'] = ypath
-                    if metafile:
-                        metafile_list.append(metafile)
+                    if not valid:
+                        validate_yaml.print_validation_report(
+                            metafile, missing_mandatory_keys, invalid_keys,
+                            invalid_entries)
+                    else:
+                        if metafile:
+                            metafile_list.append(metafile)
     return metafile_list
