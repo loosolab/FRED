1820195dde22c26a236613a085926c7b8a9d21e1
Jasmin Walter <jasmin.walter@mni.thm.de>
2022-05-11 09:54:09 +0200

    adjusted functions to new whitelist type + fix output

--- src/generate.py
+++ src/generate.py
@@ -3,12 +3,11 @@ import src.validate_yaml as validate_yaml
 import datetime
 
 factor = []
-not_editable = ['id', 'project_name', 'sample_name', 'pooled', 'donor_count']
+not_editable = ['id', 'project_name', 'sample_name', 'pooled', 'donor_count',
+                'technical_replicates']
 
 
 # TODO test if file exists for ID
-# TODO validation
-# TODO save yaml file as id_metadata.yaml
 def generate_file(input_id, name, mandatory_mode):
     """
     This function is used to generate metadata by calling functions to compute
@@ -25,20 +24,44 @@ def generate_file(input_id, name, mandatory_mode):
         if item in result_dict:
             result_dict[item] = {**result_dict[item],
                                  **get_redo_value(key_yaml[item], item, False,
-                                                  mandatory_mode)}
+                                                  mandatory_mode, result_dict)}
         else:
             result_dict[item] = get_redo_value(key_yaml[item], item, False,
-                                               mandatory_mode)
-    #valid, missing_mandatory_keys, invalid_keys, \
-    #invalid_entries = validate_yaml.validate_file(result_dict)
-    #if not valid:
-    #    validate_yaml.print_validation_report(
-    #        result_dict, missing_mandatory_keys, invalid_keys,
-    #        invalid_entries)
+                                               mandatory_mode, result_dict)
+    print(f'{"".center(80, "-")}\n'
+          f'{"SUMMARY".center(80, " ")}\n')
+    print_summary(result_dict, '')
+    print(f'\n\n')
+    print(f'{"".center(80, "-")}\n')
+    valid, missing_mandatory_keys, invalid_keys, \
+    invalid_entries = validate_yaml.validate_file(result_dict)
+    if not valid:
+        validate_yaml.print_validation_report(
+            result_dict, missing_mandatory_keys, invalid_keys,
+            invalid_entries)
     utils.save_as_yaml(result_dict, f'{input_id}_metadata.yaml')
 
 
-def generate_part(node, key, return_dict, optional, mandatory_mode):
+def print_summary(result, pre):
+    if isinstance(result, dict):
+        print(f'{pre}')
+        tabs = '\t' * pre.count('\t')
+        for key in result:
+            print_summary(result[key], f'\t{tabs}{key}: ')
+    elif isinstance(result, list):
+        print(f'{pre}')
+        tabs = '\t' * pre.count('\t')
+        for item in result:
+            if isinstance(item, dict):
+                print_summary(item, f'{tabs}')
+            else:
+                print_summary(item, f'{tabs}\t-')
+    else:
+        print(f'{pre}{result}')
+
+
+def generate_part(node, key, return_dict, optional, mandatory_mode,
+                  result_dict):
     if isinstance(node, dict):
         optionals = []
         for item in node:
@@ -46,14 +69,15 @@ def generate_part(node, key, return_dict, optional, mandatory_mode):
             if item == 'conditions':
                 return_dict[item] = get_conditions(
                     return_dict['experimental_factors'], node[item][4],
-                    mandatory_mode)
+                    mandatory_mode, return_dict)
             elif item == 'experimental_factors':
-                return_dict[item] = get_experimental_factors(node)
+                return_dict[item] = get_experimental_factors(node, return_dict)
             elif item not in not_editable:
                 if node[item][0] == 'mandatory':
                     return_dict[item] = get_redo_value(node[item], item,
                                                        optional,
-                                                       mandatory_mode)
+                                                       mandatory_mode,
+                                                       return_dict)
                 else:
                     if item not in factor:
                         optionals.append(item)
@@ -68,18 +92,19 @@ def generate_part(node, key, return_dict, optional, mandatory_mode):
                 for option in options:
                     return_dict[option] = get_redo_value(node[option], option,
                                                          optional,
-                                                         mandatory_mode)
+                                                         mandatory_mode,
+                                                         result_dict)
     else:
         if node[0] == 'mandatory' or optional:
             value = enter_information(node, key, return_dict, optional,
-                                      mandatory_mode)
+                                      mandatory_mode, result_dict)
             return value
     return return_dict
 
 
 def print_option_list(options):
     print_options = "\n".join(
-                [f"{i + 1}: {options[i]}" for i in range(len(options))])
+        [f"{i + 1}: {options[i]}" for i in range(len(options))])
     print(f'{print_options}')
 
 
@@ -90,7 +115,7 @@ def parse_input_list(options, terminable):
     else:
         if isinstance(options, list):
             try:
-                input_list = [options[int(i.strip())-1] for i in
+                input_list = [options[int(i.strip()) - 1] for i in
                               input_list.split(',')]
             except (IndexError, ValueError) as e:
                 print(f'Invalid entry, try again:')
@@ -111,23 +136,23 @@ def parse_input_list(options, terminable):
     return input_list
 
 
-def get_redo_value(node, item, optional, mandatory_mode):
+def get_redo_value(node, item, optional, mandatory_mode, result_dict):
     if node[1]:
         redo = True
         value = []
         while redo:
             value.append(
-                generate_part(node, item, {}, optional, mandatory_mode))
-            print(f'Do you want to add another {item}?')
-            print_option_list(['Yes', 'No'])
-            redo = parse_list_choose_one([True, False])
+                generate_part(node, item, {}, optional, mandatory_mode,
+                              result_dict))
+            redo = parse_list_choose_one([True, False], f'Do you want to add another {item}?')
     else:
-        value = generate_part(node, item, {}, optional, mandatory_mode)
+        value = generate_part(node, item, {}, optional, mandatory_mode,
+                              result_dict)
     return value
 
 
-def get_experimental_factors(node):
-    factor_list = utils.read_whitelist('factor')
+def get_experimental_factors(node, result_dict):
+    factor_list, dependable = utils.read_whitelist('factor')
     print(
         f'Please select the analyzed experimental factors '
         f'(1-{len(factor_list)}) divided by comma:\n')
@@ -139,7 +164,20 @@ def get_experimental_factors(node):
         factor_value = {'factor': fac}
         fac_node = list(utils.find_keys(node, fac))[0]
         if fac_node[5]:
-            value_list = utils.read_whitelist(fac)
+            value_list, dependable = utils.read_whitelist(fac)
+            while dependable:
+                whitelist_key = value_list['ident_key']
+                value = list(utils.find_keys(result_dict, whitelist_key))
+
+                if len(value) > 1:
+                    print("ERROR: multiple values")
+
+                if value[0] in value_list:
+                    value_list = value_list[value[0]]
+                    dependable = False
+                else:
+                    value_list, dependable = utils.read_whitelist(value[0])
+
             print(
                 f'Please select the values for experimental factor '
                 f'{factor_value["factor"]} (1-{len(value_list)}) divided by '
@@ -148,8 +186,9 @@ def get_experimental_factors(node):
             used_values = parse_input_list(value_list, False)
         else:
             value_type = fac_node[7]
-            print(f'Please enter a list of {value_type} values divided by '
-                  f'comma:\n')
+            print(
+                f'Please enter a list of {value_type} values for experimental'
+                f' factor {factor_value["factor"]} divided by comma:\n')
             used_values = parse_input_list(value_type, False)
         factor_value['value'] = used_values
         experimental_factors.append(factor_value)
@@ -159,38 +198,38 @@ def get_experimental_factors(node):
     return experimental_factors
 
 
-def get_conditions(factors, node, mandatory_mode):
+def get_conditions(factors, node, mandatory_mode, result_dict):
     combinations = get_condition_combinations(factors)
     print(
         f'Please select the analyzed combinations of experimental factors '
         f'(1-{len(combinations)}) divided by comma:\n')
     print_option_list(combinations)
     used_combinations = parse_input_list(combinations, False)
-    conditions = get_replicate_count(used_combinations, node, mandatory_mode)
+    conditions = get_replicate_count(used_combinations, node, mandatory_mode,
+                                     result_dict)
     return conditions
 
 
-def get_replicate_count(conditions, node, mandatory_mode):
+def get_replicate_count(conditions, node, mandatory_mode, result_dict):
     condition_infos = []
     input_pooled = None
     for condition in conditions:
         print(f'{"".center(80, "_")}\n\n'
               f'{f"Condition: {condition}".center(80, " ")}\n'
               f'{"".center(80, "_")}\n\n'
-              f'Please enter the number of replicates:')
-        bio = parse_input_value('biological replicates', False, 'int')
-        tech = parse_input_value('technical replicates', False, 'int')
-        if bio > 0 or tech > 0:
-            print(f'Are the samples pooled?')
-            print_option_list(['Yes', 'No'])
-            input_pooled = parse_list_choose_one([True, False])
+              f'Please enter the number of biological replicates:')
+        bio = parse_input_value('count', False, 'int',
+                                result_dict)
+        if bio > 0:
+            input_pooled = parse_list_choose_one([True, False], f'Are the samples pooled?')
         condition_infos.append(
-            get_replicates(condition, bio, tech, input_pooled, node,
-                           mandatory_mode))
+            get_replicates(condition, bio, input_pooled, node,
+                           mandatory_mode, result_dict))
     return condition_infos
 
 
-def get_replicates(condition, bio, tech, input_pooled, node, mandatory_mode):
+def get_replicates(condition, bio, input_pooled, node, mandatory_mode,
+                   result_dict):
     replicates = {'condition_name': condition}
 
     if bio > 0:
@@ -198,19 +237,12 @@ def get_replicates(condition, bio, tech, input_pooled, node, mandatory_mode):
               f'\033[1m{"Biological Replicates".center(80, " ")}\033[0m\n')
         replicates['biological_replicates'] = fill_replicates(
             'biological_replicates', condition, 1, bio + 1, input_pooled, node,
-            mandatory_mode)
-
-    if tech > 0:
-        print(f'{"".center(80, "_")}\n\n'
-              f'\033[1m{"Technical Replicates".center(80, " ")}\033[0m\n')
-        replicates['technical_replicates'] = fill_replicates(
-            'technical_replicates', condition, bio + 1, bio + tech + 1,
-            input_pooled, node, mandatory_mode)
+            mandatory_mode, result_dict)
     return replicates
 
 
 def fill_replicates(type, condition, start, end, input_pooled, node,
-                    mandatory_mode):
+                    mandatory_mode, result_dict):
     conditions = condition.split('-')
     replicates = {'count': end - start, 'samples': []}
     for i in range(start, end):
@@ -220,18 +252,32 @@ def fill_replicates(type, condition, start, end, input_pooled, node,
         print(f'{f"Sample: {sample_name}".center(80, "-")}\n')
         samples['pooled'] = input_pooled
         if input_pooled:
-            donor_count = parse_input_value('donor_count', False, 'int')
+            donor_count = parse_input_value('donor_count', False, 'int',
+                                            result_dict)
         else:
             donor_count = 1
         samples['donor_count'] = donor_count
+        samples['technical_replicates'] = get_technical_replicates(sample_name)
         for cond in conditions:
             key_val = cond.split(':')
             samples[key_val[0]] = key_val[1]
         samples = {**samples,
                    **generate_part(node[type][4]['samples'][4], 'samples', {},
-                                   False, mandatory_mode)}
+                                   False, mandatory_mode, result_dict)}
         replicates['samples'].append(samples)
     return replicates
+
+
+def get_technical_replicates(sample_name):
+    print(f'Please enter the number of technical replicates:')
+    count = parse_input_value('count', False, 'int', [])
+    while count < 1:
+        print(f'The number of technical replicates has to be at least 1.')
+        count = parse_input_value('count', False, 'int', [])
+    samples = []
+    for i in range(count):
+        samples.append(f'{sample_name}_t{i + 1}')
+    return {'count': count, 'sample_name': samples}
 
 
 def get_condition_combinations(factors):
@@ -252,32 +298,47 @@ def get_condition_combinations(factors):
     return combinations
 
 
-def enter_information(node, key, return_dict, optional, mandatory_mode):
+def enter_information(node, key, return_dict, optional, mandatory_mode,
+                      result_dict):
     if isinstance(node[4], dict):
         print(f'Please enter information about the {key}')
         return generate_part(node[4], key, return_dict, optional,
-                             mandatory_mode)
+                             mandatory_mode, result_dict)
     else:
-        return parse_input_value(key, node[5], node[7])
+        return parse_input_value(key, node[5], node[7], result_dict)
 
 
-def parse_list_choose_one(whitelist):
+def parse_list_choose_one(whitelist, header):
     try:
-        value = whitelist[int(input())-1]
+        print(header)
+        print_option_list(whitelist)
+        value = whitelist[int(input()) - 1]
     except (IndexError, ValueError) as e:
         print(f'Invalid entry. Please enter a number between 1 and '
               f'{len(whitelist)}')
-        value = parse_list_choose_one(whitelist)
+        value = parse_list_choose_one(whitelist, header)
     return value
 
 
-def parse_input_value(key, whitelist, value_type):
+def parse_input_value(key, whitelist, value_type, result_dict):
     whites = None
     if whitelist:
-        whites = utils.read_whitelist(key)
-    if whites:
-        print_option_list(whites)
-        input_value = parse_list_choose_one(whites)
+        whites, dependable = utils.read_whitelist(key)
+        while dependable:
+            whitelist_key = whites['ident_key']
+            value = list(utils.find_keys(result_dict, whitelist_key))
+
+            if len(value) > 1:
+                print("ERROR: multiple values")
+
+            if value[0] in whites:
+                whites = whites[value[0]]
+                dependable = False
+            else:
+                whites, dependable = utils.read_whitelist(value[0])
+
+    if whites and len(whites) > 0:
+        input_value = parse_list_choose_one(whites, f'{key}:')
     else:
         input_value = input(f'{key}: ')
         if input_value == '':
@@ -298,7 +359,8 @@ def parse_input_value(key, whitelist, value_type):
         elif value_type == 'date':
             try:
                 input_date = input_value.split('.')
-                if len(input_date) != 3:
+                if len(input_date) != 3 or len(input_date[0]) != 2 or len(
+                        input_date[1]) != 2 or len(input_date[2]) != 4:
                     raise SyntaxError
                 input_value = datetime.date(int(input_date[2]),
                                             int(input_date[1]),
@@ -306,5 +368,6 @@ def parse_input_value(key, whitelist, value_type):
                 input_value = input_value.strftime("%d.%m.%Y")
             except (IndexError, ValueError, SyntaxError) as e:
                 print(f'Input must be of type \'DD.MM.YYYY\'.')
-                input_value = parse_input_value(key, whitelist, value_type)
+                input_value = parse_input_value(key, whitelist, value_type,
+                                                result_dict)
     return input_value
