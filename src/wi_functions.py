6e48e7355102caab62c68c033b8373b60421375d
Jasmin Walter <jasmin.walter@mni.thm.de>
2022-05-12 13:16:48 +0200

    added value_unit as input_type

--- src/wi_functions.py
+++ src/wi_functions.py
@@ -25,15 +25,45 @@ def parse_empty(node, pre):
         input_fields = []
         for key in node[4]:
             input_fields.append(parse_empty(node[4][key], pre + ':' + key))
-        res = {'position': pre,
-               'mandatory': True if node[0] == 'mandatory' else False,
-               'list': node[1], 'title': node[2], 'desc': node[3],
-               'input_fields': input_fields, 'editable': editable}
+        unit = False
+        value = False
+        unit_whitelist = []
+        if len(input_fields) == 2:
+            for i in range(len(input_fields)):
+                if input_fields[i]['position'].split(':')[-1] == 'unit':
+                    unit = True
+                    unit_whitelist = input_fields[i]['whitelist']
+                elif input_fields[i]['position'].split(':')[-1] == 'value':
+                    value = True
+        if unit and value:
+
+            res = {'position': pre,
+                   'mandatory': True if node[0] == 'mandatory' else False,
+                   'list': node[1], 'displayName': node[2], 'desc': node[3],
+                   'value':None, 'value_unit': None,
+                   'whitelist': unit_whitelist, 'input_type': 'value_unit',
+                   'editable': editable}
+        else:
+            res = {'position': pre,
+                   'mandatory': True if node[0] == 'mandatory' else False,
+                   'list': node[1], 'title': node[2], 'desc': node[3],
+                   'input_fields': input_fields, 'editable': editable}
         if node[1]:
             res['list_value'] = []
     else:
         if node[5]:
-            whitelist = utils.read_whitelist(pre.split(':')[-1])
+            whitelist, depend = utils.read_whitelist(pre.split(':')[-1])
+            if depend:
+                new_white = {}
+                possible_input, depend = utils.read_whitelist(
+                    whitelist['ident_key'])
+                for key in possible_input:
+                    if key in whitelist:
+                        new_white[key] = whitelist[key]
+                    else:
+                        w, d = utils.read_whitelist(key)
+                        new_white[key] = w
+                whitelist = new_white
         else:
             whitelist = None
         res = {'position': pre,
@@ -54,10 +84,12 @@ def get_samples(condition):
                           'experimental_setting:conditions:biological_'
                           'replicates:samples')['input_fields']
     for i in range(len(samples)):
-        if samples[i]['position'] == 'experimental_setting:conditions:biological_replicates:samples:sample_name':
+        if samples[i][
+            'position'] == 'experimental_setting:conditions:biological_replicates:samples:sample_name':
             samples[i]['value'] = condition
         for c in conds:
-            if samples[i]['position'] == f'experimental_setting:conditions:biological_replicates:samples:{c.split(":")[0]}':
+            if samples[i][
+                'position'] == f'experimental_setting:conditions:biological_replicates:samples:{c.split(":")[0]}':
                 samples[i]['value'] = c.split(":")[1]
                 samples[i]['editable'] = False
     return samples
