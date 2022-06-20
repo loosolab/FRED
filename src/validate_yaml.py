8b8616ea7b14e6006c7f0f68d987e67379117483
Jasmin Walter <jasmin.walter@mni.thm.de>
2022-06-20 18:35:10 +0200

    removed unused functions

--- src/validate_yaml.py
+++ src/validate_yaml.py
@@ -23,13 +23,13 @@ def validate_file(metafile):
     """
     valid = True
     key_yaml = utils.read_in_yaml('keys.yaml')
-    invalid_keys, invalid_entries = new_test(metafile, key_yaml, [], '', [], [])
-    # invalid_keys, invalid_entries = test_for_valid_keys(metafile, key_yaml)
+    invalid_keys, invalid_entries = new_test(metafile, key_yaml, [], '', [],
+                                             [])
     missing_mandatory_keys = test_for_mandatory(metafile, key_yaml,
                                                 [x.split(':')[-1] for x in
                                                  invalid_keys])
     if len(missing_mandatory_keys) > 0 or len(invalid_keys) > 0 or len(
-           invalid_entries) > 0:
+            invalid_entries) > 0:
         valid = False
     return valid, missing_mandatory_keys, invalid_keys, invalid_entries
 
@@ -52,6 +52,10 @@ def print_validation_report(metafile, missing_mandatory_keys, invalid_keys,
         input_id = metafile['project']['id']
     except KeyError:
         input_id = 'missing'
+    try:
+        path = metafile['path']
+    except KeyError:
+        path = 'missing'
     invalid_entries = '\n- '.join(invalid_keys)
     missing = '\n- '.join(missing_mandatory_keys)
     values = []
@@ -61,7 +65,7 @@ def print_validation_report(metafile, missing_mandatory_keys, invalid_keys,
         values.append(entry + ' in ' + key + '\n')
     print(f'{"INVALID FILE".center(80, "-")}\n'
           f'Project ID: {input_id}\n'
-          f'Path: {metafile["path"]}\n\n'
+          f'Path: {path}\n\n'
           f'Report:\n')
     if len(invalid_keys) > 0:
         print(f'The following keys were invalid:\n'
@@ -77,24 +81,33 @@ def print_validation_report(metafile, missing_mandatory_keys, invalid_keys,
 
 # ---------------------------------UTILITIES------------------------------------
 
-def new_test(metafile, key_yaml, sub_lists, key_name, invalid_keys, invalid_entry):
-    if isinstance(metafile, dict) and not ('value' in metafile and 'unit' in metafile):
+def new_test(metafile, key_yaml, sub_lists, key_name, invalid_keys,
+             invalid_entry):
+    if isinstance(metafile, dict) and not (
+            'value' in metafile and 'unit' in metafile):
         for key in metafile:
             if not key_yaml:
                 invalid_keys.append(key)
             elif key not in key_yaml:
                 invalid_keys.append(key)
             else:
-                res_keys, res_entries = new_test(metafile[key], key_yaml[key][4], sub_lists, f'{key_name}:{key}' if key_name != '' else key, invalid_keys, invalid_entry)
+                res_keys, res_entries = new_test(metafile[key],
+                                                 key_yaml[key][4], sub_lists,
+                                                 f'{key_name}:{key}' if
+                                                 key_name != '' else key,
+                                                 invalid_keys, invalid_entry)
                 invalid_keys = res_keys
     elif isinstance(metafile, list):
         for item in metafile:
             sub_lists.append(item)
-            res_keys, res_entries = new_test(item, key_yaml, sub_lists, key_name, invalid_keys, invalid_entry)
+            res_keys, res_entries = new_test(item, key_yaml, sub_lists,
+                                             key_name, invalid_keys,
+                                             invalid_entry)
             invalid_keys = res_keys
             sub_lists = sub_lists[:-1]
     else:
-        invalid = new_test_for_whitelist(key_name.split(':')[-1], metafile, sub_lists)
+        invalid = new_test_for_whitelist(key_name.split(':')[-1], metafile,
+                                         sub_lists)
         if invalid:
             invalid_entry.append(f'{key_name}:{metafile}')
     return invalid_keys, invalid_entry
@@ -103,7 +116,8 @@ def new_test(metafile, key_yaml, sub_lists, key_name, invalid_keys, invalid_entr
 def new_test_for_whitelist(entry_key, entry_value, sublists):
     whitelist = utils.read_whitelist(entry_key)
     if isinstance(whitelist, dict):
-        while isinstance(whitelist, dict) and whitelist['whitelist_type'] == 'depend':
+        while isinstance(whitelist, dict) and whitelist[
+                'whitelist_type'] == 'depend':
             whitelist_key = whitelist['ident_key']
             for i in reversed(range(len(sublists))):
 
@@ -120,55 +134,18 @@ def new_test_for_whitelist(entry_key, entry_value, sublists):
                 whitelist = whitelist[value[0]]
             else:
                 whitelist = utils.read_whitelist(value[0])
-        if isinstance(whitelist, dict) and whitelist['whitelist_type'] == 'group':
+        if isinstance(whitelist, dict) and whitelist[
+                'whitelist_type'] == 'group':
             whitelist = [x for xs in list(whitelist.values()) for x in xs]
-    if whitelist and not isinstance(whitelist, list) and not isinstance(whitelist, dict) \
+    if whitelist and not isinstance(whitelist, list) and not isinstance(
+            whitelist, dict) \
             and os.path.isfile(os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), '..', 'whitelists',
-        whitelist)):
+            os.path.dirname(os.path.abspath(__file__)), '..', 'whitelists',
+            whitelist)):
         whitelist = utils.read_whitelist(whitelist)
     if whitelist and entry_value not in whitelist:
         return True
     return False
-
-def test_for_valid_keys(metafile, key_yaml):
-    """
-    This function checks if all keys in the metadata file are valid. It also
-    calls a function to test if the entered values match the whitelist.
-    :param metafile: the read in metadata yaml file
-    :param key_yaml: the read in structure file 'keys.yaml'
-    :return:
-    invalid_keys: a list containing invalid keys
-    invalid_entry: a list containing invalid entries -> (key, [value])
-    """
-    file_keys = utils.get_keys_in_dict(metafile, {}, '')
-    invalid_keys = []
-    invalid_entry = []
-    for key in file_keys:
-        item = utils.find_list_key(key_yaml, key)
-        if len(item) == 0:
-            invalid_keys.append(key)
-        elif item[0][5]:
-            whitelist = test_for_whitelist(key.split(':')[-1], file_keys[key])
-            if len(whitelist) > 0:
-                invalid_entry.append([key, whitelist])
-    return invalid_keys, invalid_entry
-
-
-def test_for_whitelist(whitelist_key, entry_key):
-    """
-    This function reads in the whitelist file for a given key and tests if the
-    entry value matches the whitelist.
-    :param whitelist_key: the key for which the whitelist should be read in
-    :param entry_key: the key of the metadata file whose value is to be tested
-    :return: invalid: a list containing the invalid keys
-    """
-    invalid = []
-    whitelist = utils.read_whitelist(whitelist_key)
-    for element in entry_key:
-        if whitelist and element not in whitelist:
-            invalid.append(element)
-    return invalid
 
 
 def test_for_mandatory(metafile, key_yaml, invalid_keys):
