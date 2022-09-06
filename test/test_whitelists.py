9dd52aa31de3b92859b9ef0fc8813387a7f054fe
Jasmin Walter <jasmin.walter@mni.thm.de>
2022-09-06 02:11:27 +0200

    fix test

--- test/test_whitelists.py
+++ test/test_whitelists.py
@@ -87,7 +87,9 @@ def testing(file, whitelist, whitelist_path, headers, depend):
 
 def testing2(file, whitelist, abbrev, headers):
     for i in range(len(whitelist)):
-        if whitelist is not None and whitelist[i] is not None and whitelist[i].lower() in abbrev:
+        print(whitelist)
+        print(abbrev)
+        if whitelist[i].lower() in abbrev:
             whitelist[i] = abbrev[whitelist[i].lower()]
 
     if not headers:
