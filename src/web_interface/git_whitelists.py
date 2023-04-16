1adc33835c6469a6f6a8c9b258961cd91908c82e
jwalter <Jasmin.Walter@mpi-bn.mpg.de>
2023-04-16 20:58:38 +0200

    fix: path for whitelist cloning

--- src/web_interface/git_whitelists.py
+++ src/web_interface/git_whitelists.py
@@ -5,16 +5,16 @@ import git
 def get_whitelists():
 
     if not os.path.exists(os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), '..',
+            os.path.dirname(os.path.abspath(__file__)), '..', '..',
             'metadata_whitelists')):
         repo = git.Repo.clone_from(
             'https://gitlab.gwdg.de/loosolab/software/metadata_whitelists.'
             'git/',
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..',
                          'metadata_whitelists'))
     else:
         repo = git.Repo(
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..',
                          'metadata_whitelists'))
         o = repo.remotes.origin
         o.pull()
