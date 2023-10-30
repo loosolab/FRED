37045e7c4c0e167929e2f1e7346149d33733b642
jwalter <Jasmin.Walter@mpi-bn.mpg.de>
2023-10-30 12:55:40 +0100

    add test print

--- src/validate_yaml.py
+++ src/validate_yaml.py
@@ -183,7 +183,7 @@ def new_test(metafile, key_yaml, sub_lists, key_name, invalid_keys,
             elif not key_yaml or (key_yaml and key not in key_yaml):
                 invalid_keys.append(f'{key_name}:{key}')
             else:
-                print(key, key_yaml, metafile)
+                print(key, key_yaml[key]['list'], metafile)
                 if key == 'factor':
                     global factor
                     factor = metafile[key]
