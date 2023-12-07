18542cc02eba827e243c046df0f5226fffbe41f2
jwalter <Jasmin.Walter@mpi-bn.mpg.de>
2023-12-07 14:52:35 +0100

    add float numbers

--- src/web_interface/wi_utils.py
+++ src/web_interface/wi_utils.py
@@ -202,7 +202,7 @@ def split_value_unit(value_unit):
     """
 
     # split value and unit
-    unit = value_unit.lstrip('0123456789')
+    unit = value_unit.lstrip('0123456789.')
     value = int(value_unit[:len(value_unit) - len(unit)])
 
     return {'value': value, 'unit': unit}
