9f6370b5196a3fc4a2c46a164bbb04766d801f3b
Noah Knoppik <noah.knoppik@mpi-bn.mpg.de>
2024-09-17 18:25:42 +0200

    readded all

--- src/run_file_reading.py
+++ src/run_file_reading.py
@@ -5,9 +5,26 @@ import os
 
 print("my exec path", os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
+spec = ilu.spec_from_file_location("file_reading", "file_reading.py")
+file_reading = ilu.module_from_spec(spec)
+spec.loader.exec_module(file_reading)
 # Set up argument parser
 parser = argparse.ArgumentParser(description="Process input and output file paths.")
 parser.add_argument("--input", required=True, help="Path to the input JSON file")
 parser.add_argument("--output", required=True, help="Path to the output JSON file")
 args = parser.parse_args()
 get_func_params = {}
+
+with open(args.input, "r") as f:
+    get_func_params = json.loads(f.read())
+
+metafiles, validation_reports = file_reading.iterate_dir_metafiles(
+    get_func_params["key_yaml"],
+    get_func_params["path_metafiles"],
+    return_false=True,
+    run_as_sub=False,
+)
+
+
+with open(args.output, "w") as f:
+    json.dump({"metafiles": metafiles, "validation_reports": validation_reports}, f)
