7e7f3dc82c7f27b45396611a2cb40b24d8bea401
Jasmin Walter <Jasmin.Walter@mpi-bn.mpg.de>
2025-06-23 12:37:12 +0200

    update plot

--- create_heatmap.py
+++ create_heatmap.py
@@ -1,16 +1,15 @@
 import plotly.graph_objects as go
-import plotly as plt
 from src import utils
 from dash_utils import *
+import plotly.express as px
 import os
 import base64
-import jinja2 
 import io
 from PIL import Image
 
 
 def get_heatmap(path, keys_yaml, show_setting_id=True, only_factors=False):
-    settings, experimental_factors, organisms, max_vals, options_pretty, annotated_dict = get_data(path, keys_yaml)
+    settings, experimental_factors, organisms, max_vals, options_pretty, annotated_dict, max_annotation = get_data(path, keys_yaml)
 
     heatmaps=[]
 
@@ -20,9 +19,15 @@ def get_heatmap(path, keys_yaml, show_setting_id=True, only_factors=False):
             sorter = experimental_factors[value]
         else:
             sorter = experimental_factors[value] + [o for o in options if o not in experimental_factors[value]]
-
         df_empty = settings[value].dropna(axis=1, how='all')
         sorter = [x for x in sorter if x in df_empty.columns]
+        val_sorter = [0]
+        for elem in sorter:
+            if elem in max_annotation[value]:
+                val_sorter.append(max(val_sorter[-1]+50, val_sorter[-1]+(15*max_annotation[value][elem])+20))
+            else:
+                val_sorter.append(val_sorter[-1]+50)
+
         df = [settings[value][f'{key}_num'] for key in sorter]   
 
         annotated = [annotated_dict[value][key] for key in sorter if key in annotated_dict[value]]
@@ -50,7 +55,14 @@ def get_heatmap(path, keys_yaml, show_setting_id=True, only_factors=False):
             else:
                 option_text.append(color("black", options_pretty[option]))
                 
-        plotly_colors = [x for x in px.colors.qualitative.G10 if x != "#DC3912"]
+        plotly_colors = []
+
+        colors_scales = [px.colors.qualitative.G10, px.colors.qualitative.Plotly, px.colors.qualitative.D3, px.colors.qualitative.T10]
+        
+        i = 0
+        while len(plotly_colors) < max_vals[value]:
+            plotly_colors += [x for x in colors_scales[i%len(colors_scales)] if x not in ["#DC3912", "#EF553B", "#D62728", "#F58518"]]
+            i += 1
 
         colors = [[0, 'white']]
         for i in range(1, max_vals[value]+1):
@@ -63,7 +75,7 @@ def get_heatmap(path, keys_yaml, show_setting_id=True, only_factors=False):
                     zmin=0,
                     zmax=max_vals[value],
                     x=[settings[value]['condition_index'],settings[value]['sample_index']],
-                    y=sorter,
+                    y=val_sorter,
                     showscale=False,
                     customdata=annotated,
                     text=label_text,
@@ -94,11 +106,12 @@ def get_heatmap(path, keys_yaml, show_setting_id=True, only_factors=False):
         data_input = heatmap
         
         my_cell_width = 150
+        #my_cell_height = max(50, (15*max_annotation[value])+20)
         top_margin = 100
         bottom_margin = 0
         left_margin = 200
         right_margin = 200
-        my_height = 50*len(sorter)
+        my_height = val_sorter[-1] #my_cell_height*len(sorter)
         my_width = my_cell_width*len(settings[value]["sample_index"])
 
         organism_path = os.path.join(os.path.dirname(__file__), 'images', f'{organisms[value]}.png')
@@ -124,6 +137,9 @@ def get_heatmap(path, keys_yaml, show_setting_id=True, only_factors=False):
                 xanchor="right", yanchor="bottom"
         )]
 
+        between = []
+        for i in range(len(val_sorter)-1):
+            between.append((val_sorter[i+1]+val_sorter[i])/2)
 
         layout = go.Layout(
             images = images,
@@ -148,37 +164,32 @@ def get_heatmap(path, keys_yaml, show_setting_id=True, only_factors=False):
             paper_bgcolor='rgba(0,0,0,0)',
             xaxis=dict(
                 side="top",
-                showline=True,
-                gridcolor="lightgrey",
                 automargin=False
             ),
             yaxis=dict(
                 tickmode="array",
                 ticktext=option_text, 
-                tickvals=sorter,
+                tickvals=between,
                 autorange="reversed",
-                tickson="boundaries",
-                showline=True,
-                gridcolor="lightgrey",
                 automargin=False
-            ),
-            legend=dict(
-                #title=f"Organism: {organisms[value]}",
-                orientation="h",
-                x=0, y=0
             )
         )    
+
         fig = go.Figure(data=data_input, layout=layout)      
-        for i in range(len(sorter)):
-            fig.add_hline(i, line_dash="dash", line_color='lightgrey',layer='below')
-            fig.add_hline(i-0.5, line_width=0.5) 
-        fig.add_hline(len(sorter)-0.5, line_width=0.5)     
+        for i in val_sorter:
+            fig.add_hline(i, line_width=0.5)
+
+        for i in between:
+            fig.add_hline(i, line_dash="dash", line_color='lightgrey',layer='below') 
+        #fig.add_hline(len(sorter)-0.5, line_width=0.5)     
 
         for i in range(len(settings[value]['sample_index'])+1):
             fig.add_vline(i-0.5, line_width=0.5)
         
-        fig.add_hrect(y0=-0.5, y1=len(experimental_factors[value])-0.5, line=dict(color="red", width=5), layer='above')
+        fig.add_hrect(y0=0, y1=val_sorter[len(experimental_factors[value])], line=dict(color="red", width=5), layer='above')
         
+        fig.update_layout(modebar={'remove':['zoom', 'pan', 'zoomIn', 'zoomOut']})
+
         table_header = ['<b>Experimental Factors</b>'] + [f'<b>{k}</b>' for k in condition_labels]
         table_values = [[f"<b>{x.replace('_', ' ').title()}</b>" for x in experimental_factors[value]]]
 
--- dash_utils.py
+++ dash_utils.py
@@ -1,166 +1,263 @@
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
-import plotly.express as px
 import pandas as pd
-import argparse
-import pathlib
 from src import utils
 import numpy as np
-import json
-import src.web_interface.factors_and_conditions as fc
 import re
 
 def get_data(path, keys_yaml):
-    yaml = path
-    keys = keys_yaml
-    settings = list(utils.find_keys(yaml, 'experimental_setting'))
+
     setting_dict = {}
     annotated_dict = {}
     experimental_factors = {}
     organisms = {}
-
-    options = [x for x in list(utils.find_keys(keys, 'samples'))[0]['value'].keys()if x not in ['sample_name', 'technical_replicates']]
     option_types = {}
     option_pretty = {}
     max_vals = {}
-    for setting in settings[0]:
-        df_dict = {}
-        setting_id = setting['setting_id']
-        organisms[setting_id] = setting['organism']['organism_name']
-        experimental_factors[setting_id] = [x['factor'] for x in setting['experimental_factors']]
+    max_anno = {}
 
-        conditions = list(utils.find_keys(setting, 'conditions'))[0]
-        condition_names = []
-        condition_index = []
-        sample_names = []
-        sample_index = []
-        idx = []
-        option_dict = {}
-        annotated = {}
-        c = 1
-        j=1
-        for cond in conditions:
+    yaml = path
+    keys = keys_yaml
+    settings = list(utils.find_keys(yaml, 'experimental_setting'))
+
+    if len(settings) > 0:
+
+        options = [x for x in list(utils.find_keys(keys, 'samples'))[0]['value'].keys()if x not in ['sample_name']]
+
+        for setting_index, setting in enumerate(settings[0]):
             
-            condition_name = cond['condition_name']
-            samples = list(utils.find_keys(cond, 'samples'))[0]
+            df_dict = {}
+            max_annotation = {}
+
+            try:
+                setting_id = setting['setting_id']
+            except KeyError:
+                setting_id = f'setting_{setting_index}'
             
-            i = 1
-            for sample in samples:
+            try:
+                organisms[setting_id] = setting['organism']['organism_name']
+            except KeyError:
+                organisms[setting_id] = None
+
+            try:
+                experimental_factors[setting_id] = [x['factor'] for x in setting['experimental_factors']]
+            except KeyError:
+                experimental_factors[setting_id] = None
+
+            conditions = list(utils.find_keys(setting, 'conditions'))
+            
+            if len(conditions) > 0:
                 
-                condition_names.append(condition_name)
-                condition_index.append(f'Condition {c}')
-                sample_name = sample['sample_name']
-                sample_index.append(f'Sample {i}')
-                idx.append(j)
-                j+=1
-                i+=1
-                sample_names.append(sample_name)
+                conditions = conditions[0]
+                condition_names = []
+                condition_index = []
+                sample_index = []
+                idx = []
+                option_dict = {}
+                annotated = {}
+                
+                j=1
+                
+                for cond_index, cond in enumerate(conditions):
+                
+                    # TODO: only table?
+                    condition_name = cond['condition_name']
+                    
+                    samples = list(utils.find_keys(cond, 'samples'))
+                    
+                    if len(samples) > 0:
+                        samples = samples[0]
+                
+                        for samp_index, sample in enumerate(samples):
+                            
+                            condition_names.append(condition_name)
+                            condition_index.append(f'Condition {cond_index+1}')
+                            
+                            sample_index.append(f'Sample {samp_index+1}')
+                            idx.append(j)
+                            j+=1
 
-                for key in options:
-                    if key not in option_dict:
-                        option_dict[key] = []
-                        annotated[key] = []
-                    if key in sample:
-                        if isinstance(sample[key], int) or isinstance(sample[key], float):
-                            option_dict[key].append(sample[key])
-                        elif (isinstance(sample[key], dict) and 'value' in sample[key] and 'unit' in sample[key]):
-                            option_dict[key].append(sample[key])
+                            for key in options:
+
+                                if key not in option_dict:
+                                    option_dict[key] = []
+                                    annotated[key] = []
+                                
+                                if key in sample:
+                                    
+                                    if key == 'technical_replicates':
+                                        try:
+                                            option_dict[key].append(sample[key]['count'])
+                                            annotated[key].append(str(sample[key]['count']))
+                                        except KeyError:
+                                            option_dict[key].append(None)
+                                            annotated[key].append('')
+                                    
+                                    else:
+
+                                        if isinstance(sample[key], int) or isinstance(sample[key], float):
+                                            option_dict[key].append(sample[key])
+                                        
+                                        elif (isinstance(sample[key], dict) and 'value' in sample[key] and 'unit' in sample[key]):
+                                            option_dict[key].append(sample[key])
+                                        
+                                        else:
+                                            option_dict[key].append(str(sample[key]))
+                                        
+                                        key_structure = list(utils.find_keys(keys, key))
+                                        group_key = None
+                                        if len(key_structure) > 0:
+                                            try:
+                                                group_key = list(key_structure[0]['special_case']['control'].keys())[0]
+                                            except:
+                                                group_key = None
+                                        annotation = annotate(sample[key], group_key)
+                                        if key in max_annotation:
+                                            max_annotation[key] = max(max_annotation[key], len(annotation))
+                                        else:
+                                            max_annotation[key] = len(annotation)
+                                        annotated[key].append('<br>'.join(annotation))
+                                else:
+                                    option_dict[key].append(None)
+                                    annotated[key].append('')
+
+                    df_dict = {'condition_name': condition_names,
+                               'condition_index': condition_index,
+                               'sample_index': sample_index,
+                               'index': idx}
+                    
+                for option in option_dict:
+
+                    option_structure = list(utils.find_keys(keys, option))
+                        
+                    if len(option_structure) > 0:
+                        option_structure = option_structure[0]
+                            
+                        if option not in option_pretty:
+                            option_pretty[option] = option.replace('_', ' ').title()
+                            
+                        if option == 'technical_replicates':
+                            option_types[option] = 'number'
+
+                        elif 'input_type' in option_structure:
+                            option_types[option] = option_structure['input_type']
+                            
+                        elif 'special_case' in option_structure and 'value_unit' in option_structure['special_case']:
+                            option_types[option] = 'value_unit'
+                            
                         else:
-                            option_dict[key].append(str(sample[key]))
-                        annotated[key].append('<br>'.join(annotate(sample[key])))
+                            option_types[option] = 'nested'
+                        
+                max_options = max([len(set([str(y) for y in option_dict[x] if y is not None])) for x in option_dict.keys() if option_types[x] not in ['number', 'value_unit']])
+                max_number = max([max([y for y in option_dict[x] if y is not None] + [1]) for x in option_dict.keys() if option_types[x] == 'number'])
+                    
+                vu_values = {}
+                    
+                for option in option_dict:
+
+                    if option_types[option] == 'value_unit':
+                            
+                        for i in range(len(option_dict[option])):
+                                
+                            if option_dict[option][i] is not None:
+                                    
+                                if option_dict[option][i]['unit'] not in vu_values:
+                                        vu_values[option_dict[option][i]['unit']] = {}
+                                    
+                                if 'vals' not in vu_values[option_dict[option][i]['unit']]:
+                                    vu_values[option_dict[option][i]['unit']]['vals'] = []
+                                    
+                                vu_values[option_dict[option][i]['unit']]['vals'].append(option_dict[option][i]['value'])
+                    
+                min_vu = 0.1
+                max_vu = 1
+                    
+                for unit in vu_values:
+                    vu_values[unit]['max'] = max_vu
+                    vu_values[unit]['min'] = min_vu
+                    min_vu += 1
+                    max_vu += 1
+                    
+                for option in option_dict:
+
+                    option_num = []
+                    if option_types[option] == 'value_unit':
+                        # TODO: sort by unit
+                        option_values = sorted([x for x in option_dict[option] if x is not None], key=lambda d: d['unit'])
                     else:
-                        option_dict[key].append(None)
-                        annotated[key].append('')
+                        option_values = sorted(list(set([x for x in option_dict[option] if x is not None])))
+                    if len(option_values) > 0:
+                        if option_types[option] == 'number':
+                            num_vals = [normalize(x, 0, max_number, 0.1, 1) for x in option_values]
+                        else:
+                            num_vals = list(np.linspace(1, len(option_values), len(option_values)))
+                        option_values = sorted([str(x) for x in option_values])
+                    for value in option_dict[option]:
+                        if value is None:
+                            option_num.append(value)
+                        elif option_types[option] == 'value_unit':
+                            option_num.append(normalize(value['value'], 0, max(vu_values[value['unit']]['vals']), vu_values[value['unit']]['min'], vu_values[value['unit']]['max']))
+                        else:
+                            option_num.append(num_vals[option_values.index(str(value))])
 
-            c+=1        
-        df_dict = {'condition_name': condition_names,
-                   'condition_index': condition_index,
-                    'sample_name': sample_names,
-                    'sample_index': sample_index,
-                    'index': idx}
-        
-        for option in option_dict:
-            option_structure = list(utils.find_keys(keys, option))[0]
-            if option not in option_pretty:
-                option_pretty[option] = option.replace('_', ' ').title()
-            if 'input_type' in option_structure:
-                option_types[option] = option_structure['input_type']
-            elif 'special_case' in option_structure and 'value_unit' in option_structure['special_case']:
-                option_types[option] = 'value_unit'
-            else:
-                option_types[option] = 'nested'
-        
-        max_options = max([len(set([str(y) for y in option_dict[x] if y is not None])) for x in option_dict.keys() if option_types[x] not in ['number', 'value_unit']])#, 'value_unit']]) 
-        max_number = max([max([y for y in option_dict[x] if y is not None] + [1]) for x in option_dict.keys() if option_types[x] == 'number'])
-        vu_values = {}
-        for option in option_dict:
-            if option_types[option] == 'value_unit':
-                for i in range(len(option_dict[option])):
-                    if option_dict[option][i] is not None:
-                        if option_dict[option][i]['unit'] not in vu_values:
-                            vu_values[option_dict[option][i]['unit']] = {}
-                        if 'vals' not in vu_values[option_dict[option][i]['unit']]:
-                            vu_values[option_dict[option][i]['unit']]['vals'] = []
-                        vu_values[option_dict[option][i]['unit']]['vals'].append(option_dict[option][i]['value'])
-        
-        min_vu = 0.1
-        max_vu = 1
-        for unit in vu_values:
-            vu_values[unit]['max'] = max_vu
-            vu_values[unit]['min'] = min_vu
-            min_vu += 1
-            max_vu += 1
-        
-        for option in option_dict:
-            option_num = []
-            if option_types[option] == 'value_unit':
-                option_values = [x for x in option_dict[option] if x is not None]
-            else:
-                option_values = list(set([x for x in option_dict[option] if x is not None]))
-            if len(option_values) > 0:
-                if option_types[option] == 'number':
-                    num_vals = [normalize(x, 0, max_number, 0.1, 1) for x in option_values]
-                else:
-                    num_vals = list(np.linspace(1, len(option_values), len(option_values)))
-                option_values = [str(x) for x in option_values]
-            for value in option_dict[option]:
-                if value is None:
-                    option_num.append(value)
-                elif option_types[option] == 'value_unit':
-                    option_num.append(normalize(value['value'], 0, max(vu_values[value['unit']]['vals']), vu_values[value['unit']]['min'], vu_values[value['unit']]['max']))
-                else:
-                    option_num.append(num_vals[option_values.index(str(value))])
+                    df_dict[option] = option_dict[option]
+                    df_dict[f'{option}_num'] = option_num
+                    
+                for option in option_dict:
+                    if option_types[option] == 'value_unit':
+                        for i in range(len(option_dict[option])):
+                            if option_dict[option][i] is not None:
+                                option_dict[option][i] = str(option_dict[option][i])
 
-            df_dict[option] = option_dict[option]
-            df_dict[f'{option}_num'] = option_num
-        
-        for option in option_dict:
-            if option_types[option] == 'value_unit':
-                for i in range(len(option_dict[option])):
-                    if option_dict[option][i] is not None:
-                        option_dict[option][i] = str(option_dict[option][i])
-
-        df = pd.DataFrame(df_dict) 
-        setting_dict[setting_id] = df
-        annotated_dict[setting_id] = annotated
-        max_vals[setting_id] = max_options
-    return setting_dict, experimental_factors, organisms, max_vals, option_pretty, annotated_dict
+                df = pd.DataFrame(df_dict) 
+                setting_dict[setting_id] = df
+                annotated_dict[setting_id] = annotated
+                max_vals[setting_id] = max_options   
+                max_anno[setting_id] = max_annotation               
+                    
+    return setting_dict, experimental_factors, organisms, max_vals, option_pretty, annotated_dict, max_anno
 
 
 def normalize(x, minIn, maxIn, minOut, maxOut):
     input = (x-minIn)/(maxIn-minIn)
     return minOut + input * (maxOut-minOut)
 
-def annotate(value):
+def annotate(value, group_key=None):
     annotated_value = []
     if isinstance(value, list):
-        for elem in value:
-            annotated_value += annotate(elem)
+        if all([isinstance(x, dict) for x in value]):
+            if group_key is not None:
+                group_vals = [x[group_key] for x in value if group_key in x]
+                grouped_value = {}
+                for val in group_vals:
+                    grouped_value[val] = [val]
+                    for elem in value:
+                        if group_key in elem and elem[group_key] == val:
+                            grouped_value[val] += [x for x in annotate(elem) if x not in grouped_value[val]]
+                for key in list(grouped_value.keys()):
+                    annotated_value += [x for x in grouped_value[key]]
+            else:
+                for elem in value:
+                    annotated_value += [x for x in annotate(elem) if x not in annotated_value]
+            keys = []
+            for elem in [list(x.keys()) for x in value]:
+                keys += elem
+            keys = list(set(keys))
+
+            for key in keys:
+                key_vals = []
+                for elem in value:
+                    if key in elem:
+                        key_vals += [x for x in annotate(elem[key]) if x not in key_vals]
+                annotated_value += [x for x in key_vals if x not in annotated_value]
+        else:
+            for elem in value:
+                annotated_value += [x for x in annotate(elem) if x not in annotated_value]
     elif isinstance(value, dict):
         if 'value' in value and 'unit' in value:
-            annotated_value.append(f'{value["value"]}{value["unit"]}')
+            annotated_value += [x for x in [f'{value["value"]}{value["unit"]}'] if x not in annotated_value]
         else:
             for key in value:
-                annotated_value += annotate(value[key])
+                annotated_value += [x for x in annotate(value[key]) if x not in annotated_value]
     else:
-        annotated_value.append(re.sub("0000(0)*", "...", str(value)))
-    return list(set(annotated_value))
+        annotated_value += [x for x in [re.sub("0000(0)*", "...", str(value))] if x not in annotated_value]
+    
+    return annotated_value
