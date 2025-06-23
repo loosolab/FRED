6838865293bf88152eca2f8340ba7bfa0e63da25
Jasmin Walter <Jasmin.Walter@mpi-bn.mpg.de>
2025-06-23 15:14:11 +0200

    update missing samples

--- src/web_interface/html_output.py
+++ src/web_interface/html_output.py
@@ -67,7 +67,7 @@ def get_summary(wi_object, key_yaml, read_in_whitelists):
                     add_plot = {'header': header, 'plot': plot[1]}
                     if elem[2] is not None:
                         add_plot['missing_samples'] = object_to_html(elem[2], 0, False)
-                plot_list.append({'title': plot[0], 'plot': template.render(input=add_plot)})
+                    plot_list.append({'title': plot[0], 'plot': template.render(input=add_plot)})
                 setting_dict[elem] = plot_list
             else:
                 setting_dict[elem] = template.render(input={'header': header, 'html': object_to_html(yaml_object[elem], 0, False)})
